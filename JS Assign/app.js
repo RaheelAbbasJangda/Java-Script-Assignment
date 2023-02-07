db0ba03245c2e49a6762e2f6837c3432596b3976
RaheelAbbasJangda <124701438+RaheelAbbasJangda@users.noreply.github.com>
2023-02-07 20:17:31 +0500

    Add files via upload

---
+++ JS Assign/app.js
@@ -0,0 +1,204 @@
+//1_Alerts
+//alert("Welcome to my page!");
+//alert("Error! Please enter a valid password.");
+//alert("Welcome to JS Land...\n Happy Coding!");
+//alert("Welcome to JS Land");
+//alert("Happy Coding!\n Prevent this page from creating additional dialogs.");
+//console.log("Hello... I can run JS through my web browser's console");
+
+//2_VariablesForStrings
+//var username;
+//var myName;
+//myName = "Sajjad Ali";
+//document.write (myName);
+
+//var message;
+//message = "Hello World";
+//alert (message);
+
+//var name= "Ali Khan";
+//var age= "17 years old";
+//var institution = "SSUET";
+//var expertise = "Mobile & Cloud Computing - Mdoule A";
+//alert (name);
+//alert (age);
+//alert (institution);
+//alert (expertise);
+//alert ("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+//3_VariablesForNumber
+//var age = "I am __ years old.";
+//alert (age);
+
+//var numberOfVisits = "You have visited this site 21 times";
+//alert (numberOfVisits);
+
+//var birthYear = 1986;
+//document.write ("My birth year is" + birthYear +"<br>"+"Data type of my declared variable is number");
+
+//var Visitorname = "Sami Khan";
+//var Producttitle = " T-Shirt(s)";
+//var Quantity = 5;
+//document.write (Visitorname + " ordered " + Quantity + Producttitle + " on XYZ Clothing Store");
+
+//4_VariableNames_legal
+
+//var Fname = "Sajjad", Lname = "Ali", Says = "Hello";
+//document.write (Fname);
+//document.write (Lname);
+//document.write (Says);
+
+//var ABC = "abc";
+//var cde = "CDE";
+//var _def = "DEF";
+//var _de1 = "de1";
+//var _d1f = "d1f";
+
+//var $BC = "abc";
+//var !de = "CDE";
+//var 1def = "DEF";
+//var #de1 = "de1";
+//var @d1f = "d1f";
+
+//5-7_MathExpression
+//var num1 = 3;
+//var num2 = 4;
+//var sum = num1 + num2;
+//alert ("Sum of 3 and 4 is" + " " + sum);
+
+//var num3 = 5;
+//var num4 = 6;
+//var substract = num4 - num3;
+//alert ("Result of substracting 6 from 5 is" + " " + substract);
+
+//var num5 = 5;
+//var num6 = 6;
+//var multiply = num5 * num6;
+//alert ("Result of multiplication 6 and 5 is" + " " + multiply);
+
+//var num7 = 6;
+//var num8 = 6;
+//var division = num8 / num7;
+//alert ("Result of division 6 and 6 is" + " " + division);
+
+//var num9 = 6;
+//var num10 = 6;
+//var modulus = num9 % num10;
+//alert ("Result of modulus 6 and 6 is" + " " + modulus);
+
+//var Cost = 600;
+//var ticketPrice = 5 * Cost;
+//document.write ("Total cost of buy 5 ticket ot a movie is " + ticketPrice + " PKR");
+
+//var i = +prompt ("insert a number for a table");
+//var j = +prompt ("insert a number for a table end");
+//var k = +prompt ("insert a number for a table start");
+//do 
+//{
+//document.write (i + " x " + k + " = " + k*i +"<br>");
+//k++;
+//}
+//while (k<=j)
+
+//var Totalmark = 1200;
+//var Markobtained = 900;
+//var Percentage = (900/1200)*100;
+//document.write ("Total marks: "+ Totalmark + "<br>" + "Marks obtained: " + Markobtained +"<br>" + "Percentage: " + Percentage+"%");
+
+//8-11Concatenation
+
+//var welcome = prompt ("Please enter your name", "Sajjad Ali");
+//alert ("Hi, " + welcome);
+
+//var i = +prompt ("insert a number for a table", 5);
+//var j = +prompt ("insert a number for a table end");
+//var k = +prompt ("insert a number for a table start");
+//do 
+//{
+//document.write (i + " x " + k + " = " + k*i +"<br>");
+//k++;
+//}
+//while (k<=j);
+
+//var i = prompt("Please enter city name", "Karachi");
+//if (i === "Karachi"){
+//alert ("Welcome to the city of light.");
+//}
+//else
+//{
+//alert ("Your city is different");
+//}
+
+//var i = prompt("Enter your gender", "Male");
+//if (i === "Male"){
+//alert ("Good Morning, Sir");
+//}
+//else
+//{
+//alert ("Good Morning, Madam");}
+
+//var i = prompt("Enter your gender", "Red");
+//if (i === "Red"){
+//alert ("Vehicle must stop");
+//}
+//else if (i === "Yellow")
+//{
+//alert ("vehicle should get ready to move");}
+//else
+//{
+//alert ("Vehicle can move now");}
+
+//12-13_IF...Else
+//var i = +prompt("Give an input number",9);
+//var b = i%3;
+//if (b > 0)
+//{
+//alert ("Numbe is not divisible by 3");
+//}
+//else 
+//{
+//alert ("Number is divislble by 3");
+//}
+
+//var i = +prompt("Give an input number",2);
+//var b = i%2;
+//if (b > 0)
+//{
+//alert ("Number is not an even one");
+//}
+//else 
+//{
+//alert ("Number is an even one");
+//}
+
+//var i = +prompt("Give an input age",18);
+//if (i > 18)
+//{
+//alert ("Old enough");
+//}
+//else 
+//{
+//alert ("Too young");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
